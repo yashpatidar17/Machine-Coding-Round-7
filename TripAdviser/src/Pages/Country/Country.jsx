b8c3e9f56359545a1a768d779adfb71f825e42b5
Yash Patidar <yashpatidar964@gmail.com>
2023-07-09 22:03:52 +0530

    New Feat

--- TripAdviser/src/Pages/Country/Country.jsx
+++ TripAdviser/src/Pages/Country/Country.jsx
@@ -1,18 +1,23 @@
-import { useContext } from "react"
-import { DataCard } from "../Continent/DataCardPro"
+import { useContext } from "react";
+import { DataCard } from "../Continent/DataCardPro";
 import { useParams } from "react-router-dom";
 import { DataContext } from "../../Context/DataContextProvider";
 
-export const Country = ()=>{
+export const Country = () => {
+  const { tripData, currentCountry, setCurrentCountry } =
+    useContext(DataContext);
+  const { ID } = useParams();
 
-    const {tripData,currentCountry,setCurrentCountry} = useContext(DataContext);
-    const {ID} = useParams();
-    console.log(currentCountry,"fnaslnd")
+  const country = currentCountry.countries.find(
+    (item) => item.id === parseInt(ID)
+  );
 
-    return(
-        <div>
-            <h2>Top Countries in  for your holiday</h2>
-            
-        </div>
-    )
-}
+  const destination = country.destinations;
+
+  return (
+    <div>
+      <h2>Top Countries in for your holiday</h2>
+      <DataCard propData={destination} />
+    </div>
+  );
+};
